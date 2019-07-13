481c9c4d8b4fdf4acace9655f9a5e2e97522538f
Дмитрий <rabolan@mail.ru>
2019-07-13 14:13:42 +0300

    Добавьте файлы проекта.

---
+++ CrmBl/Model/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmBl.Model
+{
+    /// <summary>
+    /// Класс оописывающий товар
+    /// </summary>
+    class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Count { get; set; }
+
+        /// <summary>
+        /// Переопределенный тустринг. Возразает имя товара.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
