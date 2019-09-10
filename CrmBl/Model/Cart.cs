f919ee3fffe87070a812c1d15b7d3b9b1b6c84ff
Дмитрий <rabolan@mail.ru>
2019-09-10 22:24:24 +0300

    корзину добавили

---
+++ CrmBl/Model/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmBl.Model
+{
+    /// <summary>
+    /// Класс описывает корзину
+    /// </summary>
+   public class Cart
+    {
+    }
+}
